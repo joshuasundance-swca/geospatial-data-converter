6e652a3a5ea80ef4420ab49622596fe2d25938a9
Connor Sutton <connor.sutton@swca.com>
2023-12-05 14:20:16 -0600

    better variable naming

--- tests/test_conversions.py
+++ tests/test_conversions.py
@@ -12,8 +12,8 @@ def test_coversion(in_ext: str, out_ext: str) -> None:
     test_file = f"test.{in_ext}"
     test_file_path = os.path.join(os.getcwd(), "tests", "test_data", test_file)
     with open(test_file_path, "rb") as f:
-        kml = read_file(f)
+        in_file = read_file(f)
     out_file = f"test.{output_format_dict[out_ext][0]}"
-    converted_data = convert(kml, out_file, out_ext)
+    converted_data = convert(in_file, out_file, out_ext)
     with open("test.kml", "wb") as f:
         f.write(converted_data)
